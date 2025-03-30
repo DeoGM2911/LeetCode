7a675750dfbec429debc05bb0ad14e187155ce76
Dung Tran <dung20042911@gmail.com>
2025-03-29 22:07:02 -0400

    Add one easy BT problem

---
+++ Easy/PathSum.java
@@ -0,0 +1,20 @@
+class Solution {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) return false;
+        if (root.left == null && root.right == null) return targetSum == root.val;  // If this is a leaf
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+    }
+}
